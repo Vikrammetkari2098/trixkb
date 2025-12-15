70eb91bece0e78b43a24d4da3d59102b1e88c8d5
Vikrammetkari2098 <vikrammetkari1998@gmail.com>
2025-12-15 10:47:20 +0530

    commit for chnages in article

--- app/Livewire/Document/ArticleShow.php
+++ app/Livewire/Document/ArticleShow.php
@@ -3,9 +3,21 @@
 namespace App\Livewire\Document;
 
 use Livewire\Component;
+use App\Models\Article;
 
 class ArticleShow extends Component
 {
+    public $articleId;
+    public $article;
+
+    protected $listeners = ['openArticle'];
+
+    public function openArticle($id)
+    {
+        $this->articleId = $id;
+        $this->article = Article::find($id);
+    }
+
     public function render()
     {
         return view('livewire.document.article-show');
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
@@ -14,4 +15,9 @@ class Tag extends Model
     {
         return $this->morphedByMany(Page::class, 'taggable');
     }
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, 'article_tag');
+    }
+
 }
